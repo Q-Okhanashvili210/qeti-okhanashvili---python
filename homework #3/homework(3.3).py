bd0dfeab11328cef1999c222e81cd48bfe863432
katie1 <qetevan.okhanashvili@gmail.com>
2024-09-18 22:23:34 +0400

    fix 3rd homework

--- homework #3/homework(3.3).py
+++ homework #3/homework(3.3).py
@@ -3,6 +3,6 @@ from datetime import date
 birth_year = int(input("enter year: "))
 birth_month = int(input("enter month: "))
 birth_day = int(input("enter day: "))
-new_date = date(birth_year, birth_month, birth_day).ctime()
-print(date)
+new_date = date(birth_year, birth_month, birth_day).strftime("%A")
+print(new_date)
 
